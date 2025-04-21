7dd1f8d7432669fc52091baff2519e3a142baf88
bruceathy <67832593+bruceathy@users.noreply.github.com>
2025-04-21 07:56:50 -0400

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
-import { createBrowserRouter, RouterProvider } from "react-router";
 import HomePage from "./pages/HomePage";
 import "./App.css";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <HomePage />,
-    },
-  ]);
   return (
     <>
-      <RouterProvider router={router} />
+      <HomePage />
     </>
   );
 }
